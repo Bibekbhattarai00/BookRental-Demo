05ef63ddfee4abaa2417baf6514ac5f470e6e8a3
Bibekbhattarai00 <109234865+Bibekbhattarai00@users.noreply.github.com>
2024-02-02 20:56:39 +0545

    Update AuthorDto.java

--- src/main/java/com/example/bookrental/dto/AuthorDto.java
+++ src/main/java/com/example/bookrental/dto/AuthorDto.java
@@ -20,5 +20,5 @@ public class AuthorDto {
 
     @NotNull(message = "Mobile-No cannot be empty")
     @Pattern(regexp = "^[0-9]{10}$",message = "invalid phone")
-    Long mobileNumber;
+    String mobileNumber;
 }
